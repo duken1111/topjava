974cc3ff1f9ee2b9811b9b4a1ddb593e68f51a00
dukman <dukman.box@gmail.com>
2017-03-30 15:09:11 +0300

    w3-hw2

---
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -0,0 +1,19 @@
+package ru.javawebinar.topjava.util;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Created by DLepeshko on 30.03.2017.
+ */
+public class DateTimeUtil {
+    public static final DateTimeFormatter DATE_TME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    public static <T extends Comparable<? super T>> boolean isBetween(T value, T start, T end) {
+        return value.compareTo(start) >= 0 && value.compareTo(end) <= 0;
+    }
+
+    public static String toString(LocalDateTime ldt) {
+        return ldt == null ? "" : ldt.format(DATE_TME_FORMATTER);
+    }
+}
