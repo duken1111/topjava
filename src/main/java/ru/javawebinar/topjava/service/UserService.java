fcc5bcc397b1b7a759b0c860c8c705ba6d8342a0
dukman <dukman.box@gmail.com>
2017-04-26 14:35:44 +0300

    l5-datajpa0chach

--- src/main/java/ru/javawebinar/topjava/service/UserService.java
+++ src/main/java/ru/javawebinar/topjava/service/UserService.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.service;
 
 
+import org.springframework.cache.annotation.Cacheable;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
@@ -20,7 +21,10 @@ public interface UserService {
 
     User getByEmail(String email) throws NotFoundException;
 
+
     List<User> getAll();
 
     void update(User user);
+    
+    void evictCache();
 }
--- src/main/java/ru/javawebinar/topjava/service/UserServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package ru.javawebinar.topjava.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 import ru.javawebinar.topjava.model.User;
@@ -20,11 +22,13 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository repository;
 
+    @CacheEvict(value = "users", allEntries = true)
     public User save(User user) {
         Assert.notNull(user, "user must not be null");
         return repository.save(user);
     }
 
+    @CacheEvict(value = "users", allEntries = true)
     public void delete(int id) {
         ExceptionUtil.checkNotFoundWithId(repository.delete(id), id);
     }
@@ -38,12 +42,18 @@ public class UserServiceImpl implements UserService {
         return ExceptionUtil.checkNotFound(repository.getByEmail(email), "email=" + email);
     }
 
+    @Cacheable("users")
     public List<User> getAll() {
         return repository.getAll();
     }
 
+    @CacheEvict(value = "users", allEntries = true)
     public void update(User user) {
         Assert.notNull(user, "user must not be null");
         repository.save(user);
     }
+
+    @CacheEvict(value = "users", allEntries = true)
+    @Override
+    public void evictCache() {        }
 }
